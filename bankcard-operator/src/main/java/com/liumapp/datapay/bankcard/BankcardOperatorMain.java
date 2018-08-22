53ad16a15559952635fb15545972f9cf65cc109c
liumapp <liumapp.com@gmail.com>
2018-08-22 11:46:36 +0800

    ready to get bank card operator done

---
+++ bankcard-operator/src/main/java/com/liumapp/datapay/bankcard/BankcardOperatorMain.java
@@ -0,0 +1,17 @@
+package com.liumapp.datapay.bankcard;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * file BankcardOperatorMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/8/22
+ */
+@EnableAutoConfiguration
+@Configuration
+public class BankcardOperatorMain {
+}
