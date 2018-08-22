9801f1b0b8b5a6a8e66a373d678978c80bc52854
liumapp <liumapp.com@gmail.com>
2018-08-22 20:24:25 +0800

    add test resources

---
+++ bankcard-operator/src/test/java/com/liumapp/datapay/bankcard/BankcardOperatorTest.java
@@ -0,0 +1,23 @@
+package com.liumapp.datapay.bankcard;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * file BankcardOperatorTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/8/22
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {BankcardOperatorMain.class})
+public class BankcardOperatorTest {
+
+    public void testHandle () {
+
+    }
+
+}
