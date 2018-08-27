e5859598d794cfff7a52ac9e0f1a59fa370147eb
liumapp <liumapp.com@gmail.com>
2018-08-27 14:26:26 +0800

    make real name debug model false

---
+++ government-operator/src/test/java/com/liumapp/datapay/government/GovernmentOperatroTest.java
@@ -0,0 +1,28 @@
+package com.liumapp.datapay.government;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * file GovernmentOperatroTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/8/27
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {GovernmentOperatorMain.class})
+public class GovernmentOperatroTest {
+
+    private boolean debug = true;
+
+    @Test
+    public void testHandle () {
+
+    }
+
+
+}
