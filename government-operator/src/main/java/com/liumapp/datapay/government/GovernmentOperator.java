6845d8cb8f27ba929db455ad915ac3c35c753a5f
liumapp <liumapp.com@gmail.com>
2018-08-27 10:41:43 +0800

    update pom dependencies and add government operator main class and core class

---
+++ government-operator/src/main/java/com/liumapp/datapay/government/GovernmentOperator.java
@@ -0,0 +1,18 @@
+package com.liumapp.datapay.government;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * file GovernmentOperator.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/8/27
+ */
+@Component
+public class GovernmentOperator {
+
+
+
+}
---
+++ government-operator/src/main/java/com/liumapp/datapay/government/GovernmentOperatorMain.java
@@ -0,0 +1,25 @@
+package com.liumapp.datapay.government;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * file GovernmentOperatorMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/8/27
+ */
+@EnableAutoConfiguration
+@Configuration
+public class GovernmentOperatorMain {
+
+    @Bean
+    public GovernmentOperator governmentOperator () {
+        return new GovernmentOperator();
+    }
+
+}
