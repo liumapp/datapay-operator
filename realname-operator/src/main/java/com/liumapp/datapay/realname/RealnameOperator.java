b861d9716e11b7fb1d7fb9350e2eceb9fdec8143
liumapp <liumapp.com@gmail.com>
2018-08-27 09:44:29 +0800

    ready to test real name api

--- realname-operator/src/main/java/com/liumapp/datapay/realname/RealnameOperator.java
+++ realname-operator/src/main/java/com/liumapp/datapay/realname/RealnameOperator.java
@@ -1,7 +1,17 @@
 package com.liumapp.datapay.realname;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.liumapp.datapay.realname.bean.RealnameApi;
+import com.liumapp.qtools.http.HttpTool;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * file RealnameOperator.java
@@ -13,6 +23,42 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class RealnameOperator implements InitializingBean {
+
+    @Resource(name = "realnameHttpTool")
+    private HttpTool httpTool;
+
+    @Resource
+    private RealnameApi realnameApi;
+
+    public Map<String, String> headers = new HashMap<String, String>();
+
+    public Map<String, String> querys = new HashMap<String, String>();
+
+    /**
+     * 处理银行验证信息
+     * @param name 姓名
+     * @param idcard 身份证号码
+     * @return json object  {"code":"10000","data":{"state":"1"},"seqNo":"IQ4M30TT1808230932","message":"成功"}
+     * state:1 验证结果一致 state:2 验证结果不一致 state:3 出现异常
+     */
+    public JSONObject handle (
+            String name,
+            String idcard
+    ) throws Exception {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("key", realnameApi.getAppkey());
+        params.put("name", name);
+        params.put("idcard", idcard);
+        HttpResponse response = httpTool.doPost(
+                realnameApi.getHost(),
+                realnameApi.getPath(),
+                "POST",
+                headers,
+                querys,
+                params);
+        String res = EntityUtils.toString(response.getEntity());
+        return JSON.parseObject(res);
+    }
 
     @Override
     public void afterPropertiesSet() throws Exception {
