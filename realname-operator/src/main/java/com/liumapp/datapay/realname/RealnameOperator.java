32c324a0e023fc1603ea5154e5860d79ad3ea6c0
liumapp <liumapp.com@gmail.com>
2018-08-27 09:30:53 +0800

    add realname operator main class

---
+++ realname-operator/src/main/java/com/liumapp/datapay/realname/RealnameOperator.java
@@ -0,0 +1,12 @@
+package com.liumapp.datapay.realname;
+
+/**
+ * file RealnameOperator.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/8/27
+ */
+public class RealnameOperator {
+}
