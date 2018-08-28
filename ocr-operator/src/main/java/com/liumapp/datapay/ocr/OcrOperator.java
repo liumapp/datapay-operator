8705217e6a61806b104aa89421fb3caf263600f2
liumapp <liumapp.com@gmail.com>
2018-08-28 11:22:46 +0800

    ready to test ocr

--- ocr-operator/src/main/java/com/liumapp/datapay/ocr/OcrOperator.java
+++ ocr-operator/src/main/java/com/liumapp/datapay/ocr/OcrOperator.java
@@ -1,6 +1,17 @@
 package com.liumapp.datapay.ocr;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.liumapp.datapay.ocr.bean.OcrApi;
+import com.liumapp.qtools.http.HttpTool;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * file OcrOperator.java
@@ -11,5 +22,46 @@ import org.springframework.stereotype.Component;
  * date 2018/8/28
  */
 @Component
-public class OcrOperator {
+public class OcrOperator implements InitializingBean {
+
+    @Resource(name = "ocrHttpTool")
+    private HttpTool httpTool;
+
+    @Resource
+    private OcrApi ocrApi;
+
+    public Map<String, String> headers = new HashMap<String, String>();
+
+    public Map<String, String> querys = new HashMap<String, String>();
+
+    /**
+     * OCR 识别图片文字信息
+     * @param photo 图片的Base64
+     * @return JSONObject
+     * @throws Exception exception
+     *
+     */
+    public JSONObject handle (
+            String photo
+    ) throws Exception {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("key", ocrApi.getAppkey());
+        params.put("photo", photo);
+        HttpResponse response = httpTool.doPost(
+                ocrApi.getHost(),
+                ocrApi.getPath(),
+                "POST",
+                headers,
+                querys,
+                params);
+        String res = EntityUtils.toString(response.getEntity());
+        return JSON.parseObject(res);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+
 }
