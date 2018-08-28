22048a561add0b27eeafcbb0d8458572340a4fca
liumapp <liumapp.com@gmail.com>
2018-08-28 10:53:53 +0800

    ready to handle ocr operator

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # datapay-operator
 China data pay api operator . under coding , plz don't use right now
 
-数据宝在使用涉及到图片的API接口时，它同时支持两种方式：
-
-* 传统的直接使用base64上传图片
-
-* 先上传图片到图片接口，获取图片id，在后续的接口中再根据这个id去使用图片
-
-推荐使用第二种，因为第一种随时可能被弃用
 
  
---
+++ ocr-operator/src/main/java/com/liumapp/datapay/ocr/OcrOperatorMain.java
@@ -0,0 +1,20 @@
+package com.liumapp.datapay.ocr;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * file OcrOperatorMain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2018/8/28
+ */
+@EnableAutoConfiguration
+@Configuration
+public class OcrOperatorMain {
+
+
+
+}
